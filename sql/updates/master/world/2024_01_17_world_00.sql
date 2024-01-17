eef4b26b8212085e285c1dd0b7eaf900da9e12bf
leelf00 <leelf_00@126.com>
2024-01-17 23:06:53 +0800

    [SQL/World] Fix 2024_01_17_world_00.sql

--- sql/updates/master/world/2024_01_17_world_00.sql
+++ sql/updates/master/world/2024_01_17_world_00.sql
@@ -1,5 +1,5 @@
 -- Hoptallus (56717) Creature Text
-
+DELETE FROM `creature_text` WHERE `CreatureID`=56717;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `SoundType`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
 (56717, 0, 0, 'Oh yeah!', 14, 0, 100, 0, 0, 28789, 0, 63054, 0, 'Hoptallus Aggro'),
 (56717, 0, 1, 'Gonna spins around!', 14, 0, 100, 0, 0, 0, 0, 63062, 0, 'Hoptallus Furlwind'),
